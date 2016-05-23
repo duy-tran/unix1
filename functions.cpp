e9fdeb1a202c09d463520a5d6df0f98bfeb39704
Duy <grimshady.z@gmail.com>
2016-05-23 17:15:15 +0700

    Highlight files with same basename

--- functions.cpp
+++ functions.cpp
@@ -8,10 +8,17 @@ bool showImage(string dirName) {
 	return found == 0 ? true: false;
 }
 
+void imageMenu(string dirName) {
+	cout << endl;
+}
+
 void findImageIn(string dirName) {
 	system("clear");
 	cout << "Images in directory "<<dirName<<endl;
 	if (!showImage(dirName)) {
 		cout << "No image found!\n";
+	}
+	else {
+		imageMenu(dirName);
 	}
 }
--- showImages.sh
+++ showImages.sh
@@ -1,7 +1,32 @@
-#!/bin/bash
-find "$1" -type f -exec file {} \; | awk -F: '{if ($2 ~/image/) print $1}'
+#!/usr/local/bin/bash
+result=-1
+declare -A number
+#find "$1" -type f -exec file {} \; | awk -F: '{if ($2 ~/image/) print $1}'
 for var in `find "$1" -type f -exec file {} \; | awk -F: '{if ($2 ~/image/) print $1}'`
 do
-	exit 0
+	fileName=$(basename "$var")
+	if [ -z "${number[$fileName]}" ]
+	then
+		number[$fileName]=1		
+	else
+		number[$fileName]=$((${number[$fileName]}+1))
+	fi
+	result=0
 done
-exit -1
+
+for var in `find "$1" -type f -exec file {} \; | awk -F: '{if ($2 ~/image/) print $1}'`
+do
+	if [[ ${number[$(basename "$var")]} -gt 1 ]]
+	then
+		echo -e "\e[7;96m$var\e[0m"
+	else
+		echo $var
+	fi
+done
+
+if [[ "$result" -eq -1 ]]
+then	
+	exit -1
+else
+	exit 0
+fi
